e8d0fdf29654cf26504e991202c9c9dc7700647f
Komron Ilxomjonov <komronbek.0409@gmail.com>
2025-04-02 15:25:32 +0500

    Birinchi commit

---
+++ src/main/java/aifu/project/aifu_library/AifuLibraryApplication.java
@@ -0,0 +1,13 @@
+package aifu.project.aifu_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AifuLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AifuLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aifu/project/aifu_library/AifuLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package aifu.project.aifu_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AifuLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

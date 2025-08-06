e3422cf2e5e6ff155a1ef6a33d991d658e8a4da7
Komron Ilxomjonov <komronbek.0409@gmail.com>
2025-08-06 14:18:55 +0500

    Base book ga log yozildi

--- library-web/src/main/java/aifu/project/libraryweb/service/base_book_service/BaseBookServiceImpl.java
+++ library-web/src/main/java/aifu/project/libraryweb/service/base_book_service/BaseBookServiceImpl.java
@@ -72,7 +72,8 @@ public class BaseBookServiceImpl implements BaseBookService {
         Map<String, Object> map = Util.getPageInfo(page);
         map.put("data", list);
 
-        log.info("Base book ro'yxati olindi: ro'yxat={},  pageNumber={}, pageSize={}", list, pageNumber + 1, pageSize);
+        log.info("Base book ro'yxati olindi: ro'yxat (ID)={},  pageNumber={}, pageSize={}",
+                list.stream().map(BaseBookShortDTO::id).toList(), pageNumber + 1, pageSize);
 
         return ResponseEntity.ok(new ResponseMessage(true, "Base book ro'yxati", map));
     }
@@ -201,7 +202,7 @@ public class BaseBookServiceImpl implements BaseBookService {
         map.put("data", list);
 
         log.info("Base book qidirish amalga oshirildi: query={}, field={}, pageNumber={}, pageSize={}", query, field, pageNumber + 1, pageSize);
-        log.info("Base book qidirish natijalari: {}", list);
+        log.info("Base book qidirish natijalari (ID): {}", list.stream().map(BaseBookShortDTO::id).toList());
 
         return ResponseEntity.ok(new ResponseMessage(true, "Base book ni '%s' fieldi orqali qidirish".formatted(field), map));
     }

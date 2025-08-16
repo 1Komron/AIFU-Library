4231a06778e397707e010ee467f37e4b8e088c22
Alisher <alisherrustamov32@gmail.com>
2025-08-16 20:16:42 +0500

    x

--- common-domain/src/main/java/aifu/project/common_domain/dto/student_dto/DebtorInfoDTO.java
+++ common-domain/src/main/java/aifu/project/common_domain/dto/student_dto/DebtorInfoDTO.java
@@ -1,4 +1,14 @@
 package aifu.project.common_domain.dto.student_dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DebtorInfoDTO {
+
+
 }

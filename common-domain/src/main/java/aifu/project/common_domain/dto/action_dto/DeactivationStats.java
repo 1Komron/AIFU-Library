5e15955d18d1e5fff480e34ba3d101040e2ca92a
Alisher <alisherrustamov32@gmail.com>
2025-08-16 20:23:44 +0500

    x

--- common-domain/src/main/java/aifu/project/common_domain/dto/action_dto/DeactivationStats.java
+++ common-domain/src/main/java/aifu/project/common_domain/dto/action_dto/DeactivationStats.java
@@ -1,8 +1,19 @@
 package aifu.project.common_domain.dto.action_dto;
 
+import aifu.project.common_domain.dto.student_dto.DebtorInfoDTO;
+import lombok.Data;
+
 import java.util.List;
 
-public record DeactivationStats (
-    int successfullyDeactivated,
-    List<String>  failedDeactivations
-){}
+@Data
+public class DeactivationStats {
+    private int successCount;
+    private List<DebtorInfoDTO> debtors; // QARZDORLAR RO'YXATI
+    private List<String> notFoundOrOtherErrors; // Topilmaganlar va boshqa xatolar
+
+    public DeactivationStats(int successCount, List<DebtorInfoDTO> debtors, List<String> notFoundOrOtherErrors) {
+        this.successCount = successCount;
+        this.debtors = debtors;
+        this.notFoundOrOtherErrors = notFoundOrOtherErrors;
+    }
+}

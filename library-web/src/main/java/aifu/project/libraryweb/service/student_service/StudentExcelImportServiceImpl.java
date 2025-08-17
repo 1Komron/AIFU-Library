42c45df54edfb943413fa9c0042ea8beead4f8f2
Alisher <alisherrustamov32@gmail.com>
2025-08-17 21:18:55 +0500

    xatolik bor

--- library-web/src/main/java/aifu/project/libraryweb/service/student_service/StudentExcelImportServiceImpl.java
+++ library-web/src/main/java/aifu/project/libraryweb/service/student_service/StudentExcelImportServiceImpl.java
@@ -1,11 +1,21 @@
 package aifu.project.libraryweb.service.student_service;
 
 import aifu.project.libraryweb.config.ImportStats;
+import aifu.project.libraryweb.config.ImporterColumnProperties;
+import aifu.project.libraryweb.repository.StudentRepository;
+import aifu.project.libraryweb.service.PassportHasher;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.io.InputStream;
+
+@RequiredArgsConstructor
 @Service
 public class StudentExcelImportServiceImpl implements StudentExcelImportService {
+
+    private final StudentRepository studentRepository;
+    private final ImporterColumnProperties importerColumnProperties;
+    private final PassportHasher passportHasher;
+
     @Override
     public ImportStats importStudentsFromExcel(InputStream inputStream) {
         return null;

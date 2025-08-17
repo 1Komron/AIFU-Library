86e7641aa338fdd4797e5bb2e50a2018f1bf079e
Alisher <alisherrustamov32@gmail.com>
2025-08-17 19:09:45 +0500

    DebtorReportExcelService  class cozdat

--- common-domain/src/main/java/aifu/project/common_domain/dto/action_dto/DeactivationStats.java
+++ common-domain/src/main/java/aifu/project/common_domain/dto/action_dto/DeactivationStats.java
@@ -1,15 +1,20 @@
 package aifu.project.common_domain.dto.action_dto;
 
 import aifu.project.common_domain.dto.student_dto.DebtorInfoDTO;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DeactivationStats {
     private int successCount;
-    private List<DebtorInfoDTO> debtors; // QARZDORLAR RO'YXATI
-    private List<String> notFoundOrOtherErrors; // Topilmaganlar va boshqa xatolar
+    private List<DebtorInfoDTO> debtors;
+    private List<String> notFoundOrOtherErrors;
+
+    private String reportFileName;
+    private String reportFileBase64;
 
     public DeactivationStats(int successCount, List<DebtorInfoDTO> debtors, List<String> notFoundOrOtherErrors) {
         this.successCount = successCount;
---
+++ library-web/src/main/java/aifu/project/libraryweb/service/student_service/DebtorReportExcelService.java
@@ -0,0 +1,4 @@
+package aifu.project.libraryweb.service.student_service;
+
+public class DebtorReportExcelService {
+}
